c71a5bc4cef657a2076420d9390893e88cd1477e
Indian0901 <46989343+Indian0901@users.noreply.github.com>
2019-01-25 23:07:59 +0800

    Update test_msg.py

--- Scripts/test_msg.py
+++ Scripts/test_msg.py
@@ -5,6 +5,7 @@ from Base.initdriver import get_driver
 from Base.page import Get_obj
 import pytest
 from Data.get_data import get_data
+import allure
 
 class Test_msg:
     def setup_class(self):
@@ -19,6 +20,7 @@ class Test_msg:
         self.get_obj.write_obj().send_num('13333333333')
 
     @pytest.mark.parametrize("text", get_data())
+    @allure.step(title="发送短信")
     def test_msg(self,text):
         self.get_obj.write_obj().write_text(text)
 
